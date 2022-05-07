0cc6eeae34623192ca49934f29eb6d000446bcbc
JefferMarcelino <jeffersunde72@gmail.com>
2022-05-07 10:52:12 +0200

    sending e-mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,15 @@ const app = express()
 
 app.use(express.json())
 
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "15f2e3690c7971",
+      pass: "35d02c567eba61"
+    }
+});
+
 app.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body
      
@@ -15,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
             comment,
             screenshot
         }
+    })
+
+    await transport.sendMail({
+        from: "Equipe Feedget <oi@feedback.com>",
+        to: "Jeffer Marcelino <jeffersunde72@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16PX; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join("\n")
     })
 
     return res.status(201).json({ data: feedback })
